6ba2cc5cc73e5880ae553a368f62ef163618111d
kylerhen <zqchen318@gmail.com>
2017-08-03 17:57:24 +0800

    update

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://52.40.67.78/admin/');
-define('HTTP_CATALOG', 'http://52.40.67.78/');
+define('HTTP_SERVER', 'http://127.0.0.1:8080/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1:8080/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://52.40.67.78/admin/');
-define('HTTPS_CATALOG', 'http://52.40.67.78/');
+define('HTTPS_SERVER', 'http://127.0.0.1:8080/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1:8080/');
 
 // DIR
-define('DIR_APPLICATION', '/opt/bitnami/apps/opencart/upload/admin/');
-define('DIR_SYSTEM', '/opt/bitnami/apps/opencart/upload/system/');
-define('DIR_IMAGE', '/opt/bitnami/apps/opencart/upload/image/');
-define('DIR_STORAGE', '/opt/bitnami/apps/opencart/upload/storage/');
-define('DIR_CATALOG', '/opt/bitnami/apps/opencart/upload/catalog/');
+define('DIR_APPLICATION', 'D://xampp/htdocs/oc-test/admin/');
+define('DIR_SYSTEM', 'D://xampp/htdocs/oc-test/system/');
+define('DIR_IMAGE', 'D://xampp/htdocs/oc-test/image/');
+define('DIR_STORAGE', 'D://xampp/htdocs/oc-test/storage/');
+define('DIR_CATALOG', 'D://xampp/htdocs/oc-test/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -27,7 +27,7 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'iktvzSaBJzC8');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'opencart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
