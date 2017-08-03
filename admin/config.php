db2bd119e8a2cc79ee9519f6a5fb1472d7ba83a1
zhongbq88 <zhongbq88@163.com>
2017-08-03 17:53:30 +0800

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://52.40.67.78/admin/');
-define('HTTP_CATALOG', 'http://52.40.67.78/');
+define('HTTP_SERVER', 'admin/');
+define('HTTP_CATALOG', '');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://52.40.67.78/admin/');
-define('HTTPS_CATALOG', 'http://52.40.67.78/');
+define('HTTPS_SERVER', 'admin/');
+define('HTTPS_CATALOG', '');
 
 // DIR
-define('DIR_APPLICATION', '/opt/bitnami/apps/opencart/upload/admin/');
-define('DIR_SYSTEM', '/opt/bitnami/apps/opencart/upload/system/');
-define('DIR_IMAGE', '/opt/bitnami/apps/opencart/upload/image/');
-define('DIR_STORAGE', '/opt/bitnami/apps/opencart/upload/storage/');
-define('DIR_CATALOG', '/opt/bitnami/apps/opencart/upload/catalog/');
+define('DIR_APPLICATION', '/admin/');
+define('DIR_SYSTEM', 'system/');
+define('DIR_IMAGE', 'image/');
+define('DIR_STORAGE', 'storage/');
+define('DIR_CATALOG', 'catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,11 +26,11 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'iktvzSaBJzC8');
-define('DB_DATABASE', 'opencart');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+define('DB_PREFIX', '');
 
 // OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
