9aa96088ebdc957f11bfd8dda1200651d48e6efa
kylerhen <zqchen318@gmail.com>
2017-08-25 13:03:24 +0800

    update

---
+++ catalog/controller/shopify/updateorder.php
@@ -0,0 +1,44 @@
+<?php 
+
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+require __DIR__.'/conf.php';
+
+class ControllerShopifyUpdateorder extends Controller {
+
+public function index(){
+
+	$shopify = shopify\client($this->session->data['shop'], SHOPIFY_APP_API_KEY, $this->session->data['oauth_token']);
+	$json = array();
+	try
+	{
+		//$order_id = $this->request->post['order_id'];
+		$fulfillments = array();
+		$fulfillments[] = array(
+			"created_at"=> "2017-08-25T16:09:54-04:00",
+   			"order_id"=> 5238292616,
+    		"status"=> "failure",
+    		"tracking_company"=> 'China Post',
+    		"tracking_number"=> "1Z2345",
+    		"updated_at"=> "2017-08-25T16:09:54-04:00"
+		);
+		$product = $shopify('PUT /admin/orders/5238292616.json', array(), array('fulfillments' =>$fulfillments));
+		print_r($products);
+	}
+	catch (shopify\ApiException $e)
+	{
+		# HTTP status code was >= 400 or response contained the key 'errors'
+		echo $e;
+		print_r($e->getRequest());
+		print_r($e->getResponse());
+	}
+	catch (shopify\CurlException $e)
+	{
+		# cURL error
+		echo $e;
+		print_r($e->getRequest());
+		print_r($e->getResponse());
+	}
+}
+}
+?>
