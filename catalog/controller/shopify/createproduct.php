34252535abc5092f03d675e7b81097acb71e6d79
kylerhen <zqchen318@gmail.com>
2017-08-26 18:44:32 +0800

    update

--- catalog/controller/shopify/createproduct.php
+++ catalog/controller/shopify/createproduct.php
@@ -236,8 +236,8 @@ class ControllerShopifyCreateproduct extends Controller {
 							"variants"=>$variants,
 							"images"=>$images
 		);
-		print_r($paoduct);
-		//$this->save($paoduct,$product_id,$images,$variants);
+		//print_r($paoduct);
+		$this->save($paoduct,$product_id,$images,$variants);
 	}
 	
 	public function save($paoduct,$product_id,$images,$variants){
