88609193bc474e9480676d83aca5542a0288157a
kylerhen <zqchen318@gmail.com>
2017-09-11 18:26:16 +0800

    update

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -12,11 +12,11 @@ class ControllerCommonHome extends Controller {
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
 		$data['content_top'] = $this->load->controller('common/content_top');
-		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');*/
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 
-		$this->response->setOutput($this->load->view('common/home', $data));*/
-		$this->response->redirect('https://apps.shopify.com/customdr');
+		$this->response->setOutput($this->load->view('common/home', $data));
+		//$this->response->redirect('https://apps.shopify.com/customdr');
 	}
 }
