71df1f836f585d194b150e3173713f977c733030
kylerhen <zqchen318@gmail.com>
2017-09-18 16:15:28 +0800

    update

--- admin/model/sale/production.php
+++ admin/model/sale/production.php
@@ -188,6 +188,17 @@ class ModelSaleProduction extends Model {
 			$sql .= " AND total = '" . (float)$data['filter_total'] . "'";
 		}
 		$sql .=")";
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
 		$query = $this->db->query($sql);
 
 		return $query->rows;
