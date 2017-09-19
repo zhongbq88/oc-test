4d401ba83530ad92f20fcbf8e1fdbacbfab047f2
kylerhen <zqchen318@gmail.com>
2017-09-19 10:07:27 +0800

    update

--- catalog/controller/common/connect.php
+++ catalog/controller/common/connect.php
@@ -18,15 +18,15 @@ class ControllerCommonConnect extends Controller {
 		$email = $shops[0]."@shopify.com";
 		$this->load->model('account/customer');
 		$customer = $this->model_account_customer->getCustomerByEmail($email);
-		if(empty($customer)){
+		//if(empty($customer)){
 			$url = $this->url->link('shopify/install', 'shop='. $shop);
-		}else{
+		/*}else{
 			$this->customer->login($email, $shop);
 			$this->session->data['oauth_token'] = $this->customer->getToken();
 			$this->session->data['shop'] = $shop;
 			$this->session->data['store'] = 'shopify';
 			$url = $this->url->link('commonipl/dashboard', '');
-		}		
+		}		*/
 		$this->response->redirect($url);
 	}
 	
