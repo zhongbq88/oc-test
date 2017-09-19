ff55529fa13a31ef62ee6f77989b1da1d4c24dac
kylerhen <zqchen318@gmail.com>
2017-09-19 16:40:09 +0800

    update

--- admin/controller/catalog/customerproduct.php
+++ admin/controller/catalog/customerproduct.php
@@ -38,7 +38,7 @@ class ControllerCatalogCustomerproduct extends Controller {
 		$this->load->model('tool/image');
 		foreach($products as $product){
 			$p = json_decode($product['shopify_product_json'],true);
-			print_r($p);
+			//print_r($p);
 			if(isset($p['title'])){
 				if ($p['image']) {
 					$image =$p['image']['src'] ;
@@ -47,7 +47,7 @@ class ControllerCatalogCustomerproduct extends Controller {
 				}
 				
 				if($p['variants']){
-					$price = $p['variants']['price'] ;
+					$price = $p['variants'][0]['price'] ;
 				}else{
 					$price = 0;
 				}
