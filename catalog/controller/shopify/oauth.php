ba480ba151fa313cbe26cfd7fe05a288d1da78ce
kylerhen <zqchen318@gmail.com>
2017-09-19 11:17:25 +0800

    update

--- catalog/controller/shopify/oauth.php
+++ catalog/controller/shopify/oauth.php
@@ -12,17 +12,17 @@ class ControllerShopifyOauth extends Controller {
 		$email = $shops[0]."@shopify.com";
 		$this->load->model('account/customer');
 		$customer = $this->model_account_customer->getCustomerByEmail($email);
-		if (isset($this->session->data['install']) || !isset($_GET['code']) && empty($customer))
-		{
+		$this->session->data['shop'] = $shop;
+		/*if (isset($this->session->data['install']) || !isset($_GET['code']) && empty($customer))
+		{*/
 			$permission_url = shopify\authorization_url($_GET['shop'], SHOPIFY_APP_API_KEY, array('read_products',/* 'read_customers',*/ 'write_products','read_orders', 'write_orders', 'read_fulfillments', 'write_fulfillments'),REDIRECTION_URL);
 			unset($this->session->data['install']);
 			die("<script> top.location.href='$permission_url'</script>");
-		}else{
+		/*}else{
 			$this->customer->login($email, $shop);
 			$this->session->data['oauth_token'] = $this->customer->getToken();
-			$this->session->data['shop'] = $shop;
 			$this->response->redirect($this->url->link('shopify/connect', '', true));
-		}
+		}*/
 	}
 }
 
