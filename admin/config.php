4b71667c75716767732407e38f6fde78350f9056
kylerhen <zqchen318@gmail.com>
2017-10-13 11:18:24 +0800

    update

--- admin/config.php
+++ admin/config.php
@@ -9,11 +9,11 @@ define('HTTPS_SERVER', 'http://127.0.0.1:8080/admin/');
 define('HTTPS_CATALOG', 'http://127.0.0.1:8080/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/xampp/htdocs/oc-test/admin/');
-define('DIR_SYSTEM', 'D:/xampp/htdocs/oc-test/system/');
-define('DIR_IMAGE', 'D:/xampp/htdocs/oc-test/image/');
-define('DIR_STORAGE', 'D:/xampp/htdocs/oc-test/storage/');
-define('DIR_CATALOG', 'D:/xampp/htdocs/oc-test/catalog/');
+define('DIR_APPLICATION', 'D:/xampp/htdocs/op/oc-test/admin/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/op/oc-test/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/op/oc-test/image/');
+define('DIR_STORAGE', 'D:/xampp/htdocs/op/oc-test/storage/');
+define('DIR_CATALOG', 'D:/xampp/htdocs/op/oc-test/catalog/');
 
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
--- config.php
+++ config.php
@@ -7,10 +7,10 @@ define('HTTP_SERVER', 'https://127.0.0.1/');
 define('HTTPS_SERVER', 'https://127.0.0.1/');
 
 // DIR
-define('DIR_APPLICATION', 'G:/xampp/htdocs/oc-test/catalog/');
-define('DIR_SYSTEM', 'G:/xampp/htdocs/oc-test/system/');
-define('DIR_IMAGE', 'G:/xampp/htdocs/oc-test/image/');
-define('DIR_STORAGE', 'G:/xampp/htdocs/oc-test/storage/');
+define('DIR_APPLICATION', 'D:/xampp/htdocs/op/oc-test/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/op/oc-test/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/op/oc-test/image/');
+define('DIR_STORAGE', 'D:/xampp/htdocs/op/oc-test/storage/');
 
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
