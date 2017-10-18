4f307bdae7fcc308ed22ae3ca1de433bcc61796e
kylerhen <zqchen318@gmail.com>
2017-10-18 17:13:16 +0800

    update

--- catalog/model/checkout/order.php
+++ catalog/model/checkout/order.php
@@ -345,13 +345,13 @@ class ModelCheckoutOrder extends Model {
 					}
 				}
 			}
-			$updatepayment_code ='';
+			/*$updatepayment_code ='';
 			if(!empty($payment_code)){
 				$updatepayment_code=",payment_code='" . $this->db->escape($payment_code) . "'";
 			}
-
+*/
 			// Update the DB with the new statuses
-			$this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$order_status_id . "', date_modified = NOW() WHERE order_id = '" . (int)$order_id . "'".$updatepayment_code);
+			$this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$order_status_id . "', date_modified = NOW() WHERE order_id = '" . (int)$order_id . "'");
 
 			$this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int)$order_id . "', order_status_id = '" . (int)$order_status_id . "', notify = '" . (int)$notify . "', comment = '" . $this->db->escape($comment) . "',shopping_number = '" . $this->db->escape($shopping_number) . "', shopping_method = '" . $this->db->escape($shopping_method) . "', date_added = NOW()");
 
